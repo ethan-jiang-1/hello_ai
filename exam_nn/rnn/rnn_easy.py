d0e1cb5386aa1aa365201e03a41ea2ae22e10e9c
ethan <ethan1.jiang@gmail.com>
2016-09-02 13:25:34 +0800

    trained_plus - depends on traned synapse only

--- exam_nn/rnn/rnn_easy.py
+++ exam_nn/rnn/rnn_easy.py
@@ -23,13 +23,6 @@ def sigmoid_output_to_derivative(output):
 # training dataset generation
 binary_dim = 8
 largest_number = pow(2,binary_dim)
-
-int2binary = {}
-binary = np.unpackbits(np.array([range(largest_number)],dtype=np.uint8).T,axis=1)
-for i in range(largest_number):
-    int2binary[i] = binary[i]
-
-
 # input variables
 alpha = 0.1
 input_dim = 2
@@ -37,35 +30,42 @@ hidden_dim = 16
 output_dim = 1
 
 
-# initialize neural network weights
-synapse_0 = 2*np.random.random((input_dim,hidden_dim)) - 1
-synapse_1 = 2*np.random.random((hidden_dim,output_dim)) - 1
-synapse_h = 2*np.random.random((hidden_dim,hidden_dim)) - 1
+int2binary = {}
+int2binary_init = False
+def int2bin(num):
+    global int2binary_init,int2binary
+    if not int2binary_init:
+        binary = np.unpackbits(np.array([range(largest_number)],dtype=np.uint8).T,axis=1)
+        for i in range(largest_number):
+            int2binary[i] = binary[i]
+        int2binary_init = True
+    return int2binary[num]
 
-synapse_0_update = np.zeros_like(synapse_0)
-synapse_1_update = np.zeros_like(synapse_1)
-synapse_h_update = np.zeros_like(synapse_h)
+def bin2int(bin):
+    out_int = 0
+    for index,x in enumerate(reversed(bin)):
+        out_int += x*pow(2,index)
+    return out_int
 
-# training logic
-for j in range(10000):
 
-    # generate a simple addition problem (a + b = c)
-    a_int = np.random.randint(largest_number/2) # int version
-    a = int2binary[a_int] # binary encoding
+def report_result(j,overallError,d,c,a_int,b_int):
+    print("------------")
+    print("Loop:" + str(j))
+    print("Error:" + str(overallError))
+    print("Pred:" + str(d))
+    print("True:" + str(c))
+    out_int = bin2int(d)
+    print(str(a_int) + " + " + str(b_int) + " = " + str(out_int))
+    print("")
 
-    b_int = np.random.randint(largest_number/2) # int version
-    b = int2binary[b_int] # binary encoding
 
-    # true answer
-    c_int = a_int + b_int
-    c = int2binary[c_int]
+def trained_plus(a_int,b_int,synapse_0,synapse_1,synapse_h):
+    a = int2bin(a_int)
+    b = int2bin(b_int)
 
     # where we'll store our best guess (binary encoded)
-    d = np.zeros_like(c)
+    d = np.zeros_like(a)
 
-    overallError = 0
-
-    layer_2_deltas = list()
     layer_1_values = list()
     layer_1_values.append(np.zeros(hidden_dim))
 
@@ -74,7 +74,6 @@ for j in range(10000):
 
         # generate input and output
         X = np.array([[a[binary_dim - position - 1],b[binary_dim - position - 1]]])
-        y = np.array([[c[binary_dim - position - 1]]]).T
 
         # hidden layer (input ~+ prev_hidden)
         layer_1 = sigmoid(np.dot(X,synapse_0) + np.dot(layer_1_values[-1],synapse_h))
@@ -82,53 +81,120 @@ for j in range(10000):
         # output layer (new binary representation)
         layer_2 = sigmoid(np.dot(layer_1,synapse_1))
 
-        # did we miss?... if so by how much?
-        layer_2_error = y - layer_2
-        layer_2_deltas.append((layer_2_error)*sigmoid_output_to_derivative(layer_2))
-        overallError += np.abs(layer_2_error[0])
-
         # decode estimate so we can print it out
         d[binary_dim - position - 1] = np.round(layer_2[0][0])
 
         # store hidden layer so we can use it in the next timestep
         layer_1_values.append(copy.deepcopy(layer_1))
 
-    future_layer_1_delta = np.zeros(hidden_dim)
+    out_int = bin2int(d)
 
-    for position in range(binary_dim):
+    print("plus: {0} + {1} = {2}".format(a_int,b_int,out_int))
+    return out_int
 
-        X = np.array([[a[position],b[position]]])
-        layer_1 = layer_1_values[-position-1]
-        prev_layer_1 = layer_1_values[-position-2]
-
-        # error at output layer
-        layer_2_delta = layer_2_deltas[-position-1]
-        # error at hidden layer
-        layer_1_delta = (future_layer_1_delta.dot(synapse_h.T) + \
-            layer_2_delta.dot(synapse_1.T)) * sigmoid_output_to_derivative(layer_1)
-        # let's update all our weights so we can try again
-        synapse_1_update += np.atleast_2d(layer_1).T.dot(layer_2_delta)
-        synapse_h_update += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
-        synapse_0_update += X.T.dot(layer_1_delta)
-
-        future_layer_1_delta = layer_1_delta
+def validate_trained_plus(synapse_0,synapse_1,synapse_h):
+    trained_plus(1,2,synapse_0,synapse_1,synapse_h)
+    trained_plus(11,3,synapse_0,synapse_1,synapse_h)
+    trained_plus(19,6,synapse_0,synapse_1,synapse_h)
+    trained_plus(79,86,synapse_0,synapse_1,synapse_h)
+    trained_plus(109,16,synapse_0,synapse_1,synapse_h)
 
 
-    synapse_0 += synapse_0_update * alpha
-    synapse_1 += synapse_1_update * alpha
-    synapse_h += synapse_h_update * alpha
 
-    synapse_0_update *= 0
-    synapse_1_update *= 0
-    synapse_h_update *= 0
+def rnn_run():
+    global binary_dim, largest_number
+    global alpha, input_dim, hidden_dim, output_dim
 
-    # print out progress
-    if(j % 1000 == 0):
-        print("Error:" + str(overallError))
-        print("Pred:" + str(d))
-        print("True:" + str(c))
-        out = 0
-        for index,x in enumerate(reversed(d)):
-            out += x*pow(2,index)
-        print(str(a_int) + " + " + str(b_int) + " = " + str(out))
-        print("------------")
+    # initialize neural network weights
+    synapse_0 = 2*np.random.random((input_dim,hidden_dim)) - 1
+    synapse_1 = 2*np.random.random((hidden_dim,output_dim)) - 1
+    synapse_h = 2*np.random.random((hidden_dim,hidden_dim)) - 1
+
+    synapse_0_update = np.zeros_like(synapse_0)
+    synapse_1_update = np.zeros_like(synapse_1)
+    synapse_h_update = np.zeros_like(synapse_h)
+
+    # training logic
+    for j in range(10001):
+
+        # generate a simple addition problem (a + b = c)
+        a_int = np.random.randint(largest_number/2) # int version
+        a = int2bin(a_int) # binary encoding
+
+        b_int = np.random.randint(largest_number/2) # int version
+        b = int2bin(b_int) # binary encoding
+
+        # true answer
+        c_int = a_int + b_int
+        c = int2bin(c_int)
+
+        # where we'll store our best guess (binary encoded)
+        d = np.zeros_like(c)
+
+        overallError = 0
+
+        layer_2_deltas = list()
+        layer_1_values = list()
+        layer_1_values.append(np.zeros(hidden_dim))
+
+        # moving along the positions in the binary encoding
+        for position in range(binary_dim):
+
+            # generate input and output
+            X = np.array([[a[binary_dim - position - 1],b[binary_dim - position - 1]]])
+            y = np.array([[c[binary_dim - position - 1]]]).T
+
+            # hidden layer (input ~+ prev_hidden)
+            layer_1 = sigmoid(np.dot(X,synapse_0) + np.dot(layer_1_values[-1],synapse_h))
+
+            # output layer (new binary representation)
+            layer_2 = sigmoid(np.dot(layer_1,synapse_1))
+
+            # did we miss?... if so by how much?
+            layer_2_error = y - layer_2
+            layer_2_deltas.append((layer_2_error)*sigmoid_output_to_derivative(layer_2))
+            overallError += np.abs(layer_2_error[0])
+
+            # decode estimate so we can print it out
+            d[binary_dim - position - 1] = np.round(layer_2[0][0])
+
+            # store hidden layer so we can use it in the next timestep
+            layer_1_values.append(copy.deepcopy(layer_1))
+
+        future_layer_1_delta = np.zeros(hidden_dim)
+
+        for position in range(binary_dim):
+
+            X = np.array([[a[position],b[position]]])
+            layer_1 = layer_1_values[-position-1]
+            prev_layer_1 = layer_1_values[-position-2]
+
+            # error at output layer
+            layer_2_delta = layer_2_deltas[-position-1]
+            # error at hidden layer
+            layer_1_delta = (future_layer_1_delta.dot(synapse_h.T) + \
+                layer_2_delta.dot(synapse_1.T)) * sigmoid_output_to_derivative(layer_1)
+            # let's update all our weights so we can try again
+            synapse_1_update += np.atleast_2d(layer_1).T.dot(layer_2_delta)
+            synapse_h_update += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
+            synapse_0_update += X.T.dot(layer_1_delta)
+
+            future_layer_1_delta = layer_1_delta
+
+
+        synapse_0 += synapse_0_update * alpha
+        synapse_1 += synapse_1_update * alpha
+        synapse_h += synapse_h_update * alpha
+
+        synapse_0_update *= 0
+        synapse_1_update *= 0
+        synapse_h_update *= 0
+
+        # print out progress
+        if(j % 1000 == 0):
+            report_result(j,overallError,d,c,a_int,b_int)
+
+    validate_trained_plus(synapse_0,synapse_1,synapse_h)
+
+
+rnn_run()
